319d4d000a27f997e2031f19385f00fc9ba122ba
Artem <svetilov.art@gmail.com>
2023-08-07 12:02:28 +0300

    update reviews list

--- scripts/review-scripts/main.js
+++ scripts/review-scripts/main.js
@@ -77,3 +77,124 @@ mOrderButton.addEventListener('click', () => {
     overlayOrderModal.classList.toggle('overlay--active')
 })
 
+// REVIEWS LIST
+
+const reviewsData = [
+    {
+        id: 1,
+        reviewer: 'Ярошевич Марина',
+        child: 'Сын Ярошевич Арсений (9 лет)',
+        text: 'Посещаем студию English cube c большим удовольствием. Нравится как проходит процесс обучения, дети на уроках много играют, но самое главное во время игры — запоминают новые слова. Годовая оценка в школе — 9! Ребёнок с удовольствием ходит на занятия и самостоятельно справляется с домашними заданиями в школе',
+        rating: 5,
+        answer: 'Спасибо большое, что выбрали нашу студию английского языка для детей!'
+    },
+    {
+        id: 2,
+        reviewer: 'Казакевич Марина',
+        child: 'Сын Казакевич Владислав (8 лет)',
+        text: 'Занимаемся английским языком в студии English Cube! У преподавателя Маргариты очень интересный и творческий подход к обучению! Ребёнок всегда с радостью бежит на занятия!',
+        rating: 5,
+        answer: 'Спасибо большое, что выбрали нашу студию английского языка для детей!'
+    },
+    {
+        id: 3,
+        reviewer: 'Василевская Ирина',
+        child: 'Дочь Василевская Милана (7 лет)',
+        text: 'Лучшая студия английского языка в Минске! Профессиональные педагоги, игровая методика, подарки детям за хорошую учёбу! А главное — виден результат, ребёнок читает, знает много слов, строит простые предложения и всегда бежит на занятия',
+        rating: 5,
+        answer: 'Спасибо большое, что выбрали нашу студию английского языка для детей!'
+    },
+
+]
+
+const reviews = document.querySelector('.reviews-list')
+
+function createReviewCard(authorName, authorChild, authorText, rating, answer) {
+    const name = document.createElement('div')
+    name.classList.add('name')
+    name.innerHTML = authorName
+
+    const child = document.createElement('div')
+    child.classList.add('child')
+    child.innerHTML = authorChild
+
+    const info = document.createElement('div')
+    info.classList.add('info')
+    info.appendChild(name)
+    info.appendChild(child)
+
+    const  avatar = document.createElement('div')
+    avatar.classList.add('avatar')
+
+    const author = document.createElement('div')
+    author.classList.add('author')
+    author.appendChild(avatar)
+    author.appendChild(info)
+
+    const reviewerTextPar = document.createElement('div')
+    reviewerTextPar.classList.add('reviewer-text__par')
+    reviewerTextPar.innerHTML = authorText
+
+    const reviewerRating = document.createElement('div')
+    reviewerRating.classList.add('reviewer-rating')
+    const star = document.createElement('span')
+    star.classList.add('material-symbols-rounded')
+    for(let i = 0; i < rating; i++) {
+        reviewerRating.appendChild((star))
+    }
+
+    const text = document.createElement('div')
+    text.classList.add('text')
+    text.appendChild(reviewerTextPar)
+    text.appendChild(reviewerRating)
+
+    const reviewContent = document.createElement('div')
+    reviewContent.classList.add('review-content')
+    reviewContent.appendChild(author)
+    reviewContent.appendChild(text)
+
+    const reviewItem = document.createElement('div')
+
+    // ANSWER
+    const reviewAnswer = document.createElement('div')
+    reviewAnswer.classList.add('review-answer')
+
+    const nameAns = document.createElement('div')
+    nameAns.classList.add('name')
+    nameAns.innerHTML = 'English Cube'
+
+    const childAns = document.createElement('div')
+    childAns.classList.add('child')
+    childAns.innerHTML = 'Ответ представителя'
+
+    const infoAns = document.createElement('div')
+    infoAns.classList.add('info')
+    infoAns.appendChild(nameAns)
+    infoAns.appendChild(childAns)
+
+    const  avatarAns = document.createElement('div')
+    avatarAns.classList.add('avatar')
+
+    const authorAns = document.createElement('div')
+    authorAns.classList.add('author')
+    authorAns.appendChild(avatarAns)
+    authorAns.appendChild(infoAns)
+
+    const textAns = document.createElement('div')
+    textAns.classList.add('text')
+    textAns.innerHTML = answer
+
+    reviewAnswer.appendChild(authorAns)
+    reviewAnswer.appendChild(textAns)
+    reviewItem.classList.add('review-item')
+
+    reviewItem.appendChild(reviewContent)
+    reviewItem.appendChild((reviewAnswer))
+
+    return reviewItem
+}
+reviewsData.forEach(r => {
+    const reviewCard = createReviewCard(r.reviewer, r.child, r.text, r.rating, r.answer)
+    console.log(reviewCard)
+    reviews.appendChild(reviewCard)
+})
